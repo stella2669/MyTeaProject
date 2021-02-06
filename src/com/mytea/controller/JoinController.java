64f200d0b749c7f8c02fe98bc28754ed27cfebe1
SY927 <75654117+SY927@users.noreply.github.com>
2021-02-06 17:59:10 +0900

    오류수정시도

--- src/com/mytea/controller/JoinController.java
+++ src/com/mytea/controller/JoinController.java
@@ -1,6 +1,7 @@
 package com.mytea.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import com.mytea.member.MemberDao;
 
 @WebServlet("/Join.do")
 public class JoinController extends HttpServlet {
@@ -47,6 +50,10 @@ public class JoinController extends HttpServlet {
 		String postcode = request.getParameter("postcode");
 		
 		HttpSession session = request.getSession();
+		PrintWriter out = response.getWriter();
+		
+		MemberDao dao = MemberDao.getInstance();
+		
 		
 	}
 
