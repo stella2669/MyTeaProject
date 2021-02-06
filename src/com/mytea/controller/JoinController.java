f1c644cc99dcb86838372d2f0d5df026850b49b3
SY927 <75654117+SY927@users.noreply.github.com>
2021-02-06 17:53:25 +0900

    노란줄이 생겨버림

--- src/com/mytea/controller/JoinController.java
+++ src/com/mytea/controller/JoinController.java
@@ -7,35 +7,43 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class JoinController
- */
 @WebServlet("/Join.do")
 public class JoinController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public JoinController() {
         super();
-        // TODO Auto-generated constructor stub
+        
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doAction(request,response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		doAction(request,response);
+		
+		
+	}
+	
+	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html); charset=utf-8");
+		
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		String name = request.getParameter("name");
+		String birth = request.getParameter("birth");
+		String phone = request.getParameter("phone");
+		String email1 = request.getParameter("email1");
+		String email2 = request.getParameter("email2");
+		String address1 = request.getParameter("address1");
+		String address2 = request.getParameter("address2");
+		String postcode = request.getParameter("postcode");
+		
 	}
 
 }
