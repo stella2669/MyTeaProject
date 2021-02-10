9941201073277502a3885f1c45ec79fb42066a79
stella2669 <stella2669@gmail.com>
2021-02-11 04:08:36 +0900

    CartController 생성

---
+++ src/com/mytea/controller/CartController.java
@@ -0,0 +1,37 @@
+package com.mytea.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mytea.dao.ProductDao;
+
+@WebServlet("/cart/*")
+public class CartController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nextPage = null;
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		String action = request.getPathInfo();
+		
+		//어떤 request에서 넘어왔는지 acrtion값 확인
+		System.out.println("action: " + action);
+		
+		ProductDao dao = ProductDao.getInstance();
+	
+	}
+}
