b480831d0e67c77b4b23cb33621d5800cf8a6b0e
Lee-Hyoyeon <dlgydus099@gmail.com>
2021-02-15 23:47:30 +0900

    updat

--- src/com/mytea/controller/UpdateController.java
+++ src/com/mytea/controller/UpdateController.java
@@ -1,11 +1,16 @@
 package com.mytea.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
+
 import com.mytea.dao.MemberDao;
 import com.mytea.dto.MemberDto;
 
@@ -32,20 +37,17 @@ public class UpdateController extends HttpServlet {
 	}
 
 	private void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=UTF-8"); //자바스크립트 한글깨짐 
+		
+		String id=request.getParameter("id");
+		String pw= request.getParameter("pw");
+		HttpSession session = request.getSession(); 
+        PrintWriter out = response.getWriter();
+		
+        MemberDao dao = MemberDao.getInstance();
 
-		MemberDao dao = MemberDao.getInstance();
-		
-		/*
-		 * MemberDto dto= new MemberDto(); 
-		 * MemberDto dto = dao.updateMember(dto);
-		 */
-//		String id=(String)session.getAttribute("id"); 
-//		MemberDao dao = MemberDao.getInstance();
-//		
-//		int ri= dao.updateMember(dto);  
-		
 		
 	}
 
